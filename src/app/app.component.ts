a60311d2c21cc59e7611fdc296d4b893556dce1a
florian <florian.batt@hotmail.fr>
2023-07-07 13:30:57 +0200

    Dyma chapitre 2, composants, templates, data binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { reduce } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'apptest';
+  nombre = 42;
+  bool = true;
+  obj = {
+    cle: 'valeur',
+  };
+  public square(nbr: number) {
+    return nbr * nbr;
+  }
+
+  public inputType = 'date';
+  constructor() {
+    setTimeout(() => {
+      this.inputType = 'number';
+    }, 3000);
+  }
+
+  public changeColor(color: string): void {
+    this.color = color;
+  }
+
+  public color = 'red';
+  public color2 = 'green';
+  public color3 = 'red';
+
+  public content = 'Bonjour';
+
+  public displayButton = true;
+
+  public users: Array<{ nom: string }> = [
+    {
+      nom: 'Paul',
+    },
+    {
+      nom: 'Julie',
+    },
+    {
+      nom: 'Jean',
+    },
+  ];
+
+  public box;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NouveauComponent } from './nouveau/nouveau.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NouveauComponent],
+  imports: [BrowserModule, FormsModule, NgOptimizedImage],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
