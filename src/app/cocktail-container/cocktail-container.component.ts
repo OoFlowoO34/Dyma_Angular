8aada92fc196bf18b69b5d251e1fd7f2a3c0ed53
florian <florian.batt@hotmail.fr>
2023-07-22 18:08:02 +0200

    Chapitre 16: pipes et HTTP - pipe async

--- src/app/cocktail-container/cocktail-container.component.ts
+++ src/app/cocktail-container/cocktail-container.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Cocktail } from '../shared/interfaces/cocktail.interface';
 import { CocktailService } from '../shared/services/cocktail.service';
 
@@ -8,23 +8,8 @@ import { CocktailService } from '../shared/services/cocktail.service';
   templateUrl: './cocktail-container.component.html',
   styleUrls: ['./cocktail-container.component.scss'],
 })
-export class CocktailContainerComponent implements OnInit, OnDestroy {
-  cocktails: Cocktail[] = [];
-
-  public subscription: Subscription = new Subscription();
+export class CocktailContainerComponent {
+  public cocktails$: Observable<Cocktail[]> = this.cocktailService.cocktails$;
 
   constructor(private cocktailService: CocktailService) {}
-
-  ngOnInit(): void {
-    this.subscription.add(
-      this.cocktailService.cocktails$.subscribe((cocktails: Cocktail[]) => {
-        this.cocktails = cocktails;
-      })
-    );
-
-    // this.selectedCocktail = this.cocktails[0];
-  }
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 }
